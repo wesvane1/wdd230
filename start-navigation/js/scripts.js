b1c24aa942854cad0b97a013cb5d657f8ab9f1c6
wesvane1 <wesleyvane13@gmail.com>
2023-01-25 19:28:32 -0700

    finished responsive menu assignment

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamBtn');
+x.onclick = toggleMenu;
