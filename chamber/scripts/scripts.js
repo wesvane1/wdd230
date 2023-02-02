7da3c03689f9285cf5a94f678c3c3d49755782c0
wesvane1 <wesleyvane13@gmail.com>
2023-02-02 16:08:56 -0700

    added banner that displays on mondays and tuesdays

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,3 +20,22 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 let currentYear = new Date();
 let year = currentYear.getFullYear();
 document.getElementById('currentYear').innerHTML = year;
+
+let currentDay = new Date();
+let day = currentDay.getDay();
+
+// if the day is monday or tuesday
+if (day == 4 || day == 0){
+  const headerRef = document.getElementsByTagName('header')[0];
+  let banner = document.createElement('h3');
+  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  headerRef.append(banner);
+  banner.style.textAlign = 'center';
+  banner.style.display = 'flex';
+  banner.style.backgroundColor = 'white';
+  banner.style.gridColumn = '3';
+  banner.style.gridRow = '3';
+  banner.style.margin= '10px'
+  banner.style.marginTop = '50px';
+  banner.style.borderRadius = '10px';
+}
