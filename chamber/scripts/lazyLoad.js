e800f4372629c3d26f698573d0a0d70bb779626f
wesvane1 <wesleyvane13@gmail.com>
2023-02-18 14:40:11 -0700

    Finished the discover page.

---
+++ chamber/scripts/lazyLoad.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img){
+  const src = img.getAttribute("data-src");
+  if (!src){
+    return
+  }
+  img.src = src
+}
+
+const imgOptions = {
+  threshold:0,
+  rootMargin:"0px 0px 100px 0px"
+  // the above line lets the images load 300 pixles before they show up on the users screen.
+  // if the number were to be -500, then it would load 500 pixles after the user scrolls past.
+};
+
+const imageObserver = new IntersectionObserver((entries, imageObserver) =>{
+  entries.forEach(entry =>{
+    if (!entry.isIntersecting){
+      return
+    } else {
+      preloadImage(entry.target);
+      imageObserver.unobserve(entry.target);
+    }
+  })
+}, imgOptions);
+
+images.forEach(image =>{
+  imageObserver.observe(image);
+})
