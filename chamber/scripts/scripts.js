d046aca3f1f706108f570995add50221c7059e49
wesvane1 <wesleyvane13@gmail.com>
2023-02-22 16:36:54 -0700

    removed the banner js, moved to seperate file.

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,21 +20,3 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 let currentYear = new Date();
 let year = currentYear.getFullYear();
 document.getElementById('currentYear').innerHTML = year;
-
-let currentDay = new Date();
-let day = currentDay.getDay();
-
-// if the day is monday or tuesday
-if (day == 1 || day == 2){
-  const headerRef = document.getElementsByTagName('header')[0];
-  let banner = document.createElement('h3');
-  banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  headerRef.append(banner);
-  banner.style.textAlign = 'center';
-  banner.style.backgroundColor = 'white';
-  banner.style.gridColumn = '2/5';
-  banner.style.gridRow = '3/4';
-  banner.style.margin= '10px'
-  banner.style.marginTop = '50px';
-  banner.style.borderRadius = '10px';
-}
