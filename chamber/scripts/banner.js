940404d8850c10112e3a962eae564c84716ab46c
wesvane1 <wesleyvane13@gmail.com>
2023-02-27 17:34:22 -0700

    minor CSS changes to join page.

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -7,6 +7,7 @@ if (day == 1 || day == 2){
   let banner = document.createElement('p');
   banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
   headerRef.append(banner);
+  banner.className='joinBanner';
   banner.style.textAlign = 'center';
   banner.style.backgroundColor = 'white';
   banner.style.gridRow = '8/9';
