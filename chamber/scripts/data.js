afdc7086167503f950978a54cbe198c4f0de30d7
wesvane1 <wesleyvane13@gmail.com>
2023-03-04 16:20:56 -0700

    finished directory page!

--- chamber/scripts/data.js
+++ chamber/scripts/data.js
@@ -2,7 +2,7 @@ const link = "json/data.json"
 
 const gridBtn = document.querySelector("#gridBtn");
 const listBtn = document.querySelector("#listBtn");
-const test = document.querySelector("article"); 
+const test = document.querySelector("article");
 
 
 async function getBusinessData() {
@@ -20,17 +20,22 @@ const displayBusiness = (business) =>{
   business.forEach((business) =>{
     let card = document.createElement('section');
     card.setAttribute('class', 'card');
+    let businessName = document.createElement('h3');
+    let verticalLine = document.createElement('vl');
     let businessLogo = document.createElement('img');
     let address = document.createElement('p');
     let phone = document.createElement('p');
     let url = document.createElement('a');
     let membership = document.createElement('p');
 
+    businessName.textContent = `${business.name}`
     address.textContent = `${business.address}`;
     phone.textContent = `${business.phone}`;
     url.textContent = `${business.name} Website`;
     url.href=`${url}`;
     membership.textContent = `Membership Level: ${business.membership}`;
+
+    businessName.setAttribute('class', 'businessName');
 
     businessLogo.setAttribute('src', business.businessLogo);
     businessLogo.setAttribute('id', 'businessLogo');
@@ -38,6 +43,8 @@ const displayBusiness = (business) =>{
     businessLogo.setAttribute('loading', 'lazy')
     businessLogo.setAttribute('width', '75%');
 
+    card.appendChild(businessName);
+    card.appendChild(verticalLine)
     card.appendChild(businessLogo);
     card.appendChild(address);
     card.appendChild(phone);
@@ -48,12 +55,14 @@ const displayBusiness = (business) =>{
   })
 
 
-  gridBtn.addEventListener(click, () => {
+  gridBtn.addEventListener('click', () => {
+    // console.log("grid")
     test.classList.add("grid");
     test.classList.remove("list")
   })
   
-  listBtn.addEventListener(click, () => {
+  listBtn.addEventListener('click', () => {
+    // console.log("list")
     test.classList.add("list")
     test.classList.remove("grid");
   })
