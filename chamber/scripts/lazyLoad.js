b14f3ae1255c25709180ba8a3be88a115c0a07cc
wesvane1 <wesleyvane13@gmail.com>
2023-03-15 18:30:12 -0600

    made discover page better

--- chamber/scripts/lazyLoad.js
+++ chamber/scripts/lazyLoad.js
@@ -1,31 +1,31 @@
-const images = document.querySelectorAll("img[data-src]");
+const images = document.querySelectorAll('img[data-src]')
 
-function preloadImage(img){
-  const src = img.getAttribute("data-src");
-  if (!src){
-    return
-  }
-  img.src = src
+console.log(images)
+
+const preloadImage = (img) => {
+    const src = img.getAttribute('data-src');
+    if (!src) {
+        return;
+    }
+    img.src = src
 }
 
 const imgOptions = {
-  threshold:0,
-  rootMargin:"0px 0px 100px 0px"
-  // the above line lets the images load 300 pixles before they show up on the users screen.
-  // if the number were to be -500, then it would load 500 pixles after the user scrolls past.
-};
+    // threshold: 1,
+    rootMargin: "0px 0px -100px 0px"
+}
 
-const imageObserver = new IntersectionObserver((entries, imageObserver) =>{
-  entries.forEach(entry =>{
-    if (!entry.isIntersecting){
-      return
-    } else {
-      preloadImage(entry.target);
-      imageObserver.unobserve(entry.target);
-    }
-  })
-}, imgOptions);
+const callback = (entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            preloadImage(entry.target)
+            imgObserver.unobserve(entry.target)
+        }
+    })
+}
 
-images.forEach(image =>{
-  imageObserver.observe(image);
+const imgObserver = new IntersectionObserver(callback, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image)
 })
