f29e24a2ed830465eb76bed1e22ae3d342824e47
wesvane1 <wesleyvane13@gmail.com>
2023-03-21 12:10:49 -0600

    updated JS file

--- Final Project Plan/scripts/index.js
+++ Final Project Plan/scripts/index.js
@@ -1,7 +1,3 @@
 let currentYear = new Date();
 let year = currentYear.getFullYear();
 document.getElementById("currentYear").innerHTML=year;
-
-let time = new Date();
-let lastModified = time.toLocaleString();
-document.getElementById("lastModified").innerHTML=lastModified;
