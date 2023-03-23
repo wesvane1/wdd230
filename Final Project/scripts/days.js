01df519252635e2af45f9a37151966bfad0c1c42
wesvane1 <wesleyvane13@gmail.com>
2023-03-23 07:38:20 -0600

    code now reads days correctly

--- Final Project/scripts/days.js
+++ Final Project/scripts/days.js
@@ -5,7 +5,28 @@ const day3 = document.querySelector('#nextNextDay1');
 const test = new Date();
 var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let tomorrow = test.getDay();
+console.log(tomorrow)
 
-day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
-day2.innerHTML = `<strong>${days[tomorrow+2]}</strong>`;
-day3.innerHTML = `<strong>${days[tomorrow+3]}</strong>`;
+if (tomorrow==4)
+{
+  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[tomorrow+2]}</strong>`;
+  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+}
+else if (tomorrow == 5)
+{
+  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[tomorrow-5]}</strong>`;
+  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+}
+else if (tomorrow == 6)
+{
+  day1.innerHTML = `<strong>${days[tomorrow-6]}</strong>`;
+  day2.innerHTML = `<strong>${days[tomorrow-5]}</strong>`;
+  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+}
+else{
+  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[tomorrow+2]}</strong>`;
+  day3.innerHTML = `<strong>${days[tomorrow+3]}</strong>`;
+}
