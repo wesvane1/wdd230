701bddca7087dc1d75baa7118a12bb1eee6c1a6f
wesvane1 <wesleyvane13@gmail.com>
2023-03-30 12:46:12 -0600

    changed variable names for more clarity

--- Final Project/scripts/days.js
+++ Final Project/scripts/days.js
@@ -3,29 +3,29 @@ const day2 = document.querySelector('#nextNextDay');
 const day3 = document.querySelector('#nextNextDay1');
 
 const test = new Date();
-var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let tomorrow = test.getDay();
+const days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const today = test.getDay();
 
-if (tomorrow==4)
+if (today==4)
 {
-  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
-  day2.innerHTML = `<strong>${days[tomorrow+2]}</strong>`;
-  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+  day1.innerHTML = `<strong>${days[today+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[today+2]}</strong>`;
+  day3.innerHTML = `<strong>${days[today-4]}</strong>`;
 }
-else if (tomorrow == 5)
+else if (today == 5)
 {
-  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
-  day2.innerHTML = `<strong>${days[tomorrow-5]}</strong>`;
-  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+  day1.innerHTML = `<strong>${days[today+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[today-5]}</strong>`;
+  day3.innerHTML = `<strong>${days[today-4]}</strong>`;
 }
-else if (tomorrow == 6)
+else if (today == 6)
 {
-  day1.innerHTML = `<strong>${days[tomorrow-6]}</strong>`;
-  day2.innerHTML = `<strong>${days[tomorrow-5]}</strong>`;
-  day3.innerHTML = `<strong>${days[tomorrow-4]}</strong>`;
+  day1.innerHTML = `<strong>${days[today-6]}</strong>`;
+  day2.innerHTML = `<strong>${days[today-5]}</strong>`;
+  day3.innerHTML = `<strong>${days[today-4]}</strong>`;
 }
 else{
-  day1.innerHTML = `<strong>${days[tomorrow+1]}</strong>`;
-  day2.innerHTML = `<strong>${days[tomorrow+2]}</strong>`;
-  day3.innerHTML = `<strong>${days[tomorrow+3]}</strong>`;
+  day1.innerHTML = `<strong>${days[today+1]}</strong>`;
+  day2.innerHTML = `<strong>${days[today+2]}</strong>`;
+  day3.innerHTML = `<strong>${days[today+3]}</strong>`;
 }
